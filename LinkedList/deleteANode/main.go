e710cd5f6853a5dc431e44edc5ed0e1092802036
Prajjwal <prajjwalchale18@gmail.com>
2024-04-27 19:35:02 +0530

    added changes

--- LinkedList/deleteANode/main.go
+++ LinkedList/deleteANode/main.go
@@ -1,55 +1,13 @@
 package main
 
-import "fmt"
+import (
+	ls "github.com/Prajjwal-Walchale/DSA-in-Golang/linkedList"
+)
 
-type Node struct {
-	Data int
-	Next *Node
-}
+//var Node = linkedList.Node
 
-func createALinkedList(data []int) *Node {
-	var head *Node
-	for _, value := range data {
-		newNode := &Node{Data: value}
-		if head == nil {
-			head = newNode
-		} else {
-			curr := head
-			for curr.Next != nil {
-				curr = curr.Next
-			}
-			curr.Next = newNode
-		}
-	}
-	return head
-}
-
-func PrintLinkedList(head *Node) {
-	curr := head
-	for curr != nil {
-		fmt.Print(curr.Data, " -> ")
-		curr = curr.Next
-	}
-	fmt.Print("nil\n")
-	//fmt.Println()
-}
-
-func main() {
-	data := []int{10, 20, 30, 40}
-	linkedList := createALinkedList(data)
-
-	//linkedlistTraversal
-	PrintLinkedList(linkedList)
-
-	//delete a node from last
-	deleteANodefromLast(linkedList)
-
-	//linkedlistTraversal
-	PrintLinkedList(linkedList)
-}
-
-//Delete a Node from last
-func deleteANodefromLast(head *Node) *Node {
+// Delete a Node from last
+func deleteANodefromLast(head *ls.Node) *ls.Node {
 	if head == nil {
 		return nil
 	}
@@ -64,3 +22,49 @@ func deleteANodefromLast(head *Node) *Node {
 	return head
 
 }
+
+func main() {
+	data := []int{10, 20, 30, 40}
+	linkedLst := ls.CreateLinkedList(data)
+
+	//linkedlistTraversal
+	ls.PrintLinkedList(linkedLst)
+
+	//delete a node from last
+	deleteANodefromLast(linkedLst)
+
+	//linkedlistTraversal
+	ls.PrintLinkedList(linkedLst)
+}
+
+// type Node struct {
+// 	Data int
+// 	Next *Node
+// }
+
+// func createALinkedList(data []int) *Node {
+// 	var head *Node
+// 	for _, value := range data {
+// 		newNode := &Node{Data: value}
+// 		if head == nil {
+// 			head = newNode
+// 		} else {
+// 			curr := head
+// 			for curr.Next != nil {
+// 				curr = curr.Next
+// 			}
+// 			curr.Next = newNode
+// 		}
+// 	}
+// 	return head
+// }
+
+// func PrintLinkedList(head *Node) {
+// 	curr := head
+// 	for curr != nil {
+// 		fmt.Print(curr.Data, " -> ")
+// 		curr = curr.Next
+// 	}
+// 	fmt.Print("nil\n")
+// 	//fmt.Println()
+// }
---
+++ LinkedList/go.mod
@@ -0,0 +1,3 @@
+module github.com/Prajjwal-Walchale/DSA-in-Golang
+
+go 1.20
